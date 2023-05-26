c8500d4a723baea00bd572dbf7f80564205bb3f1
Daniel Neves Fonseca <daniel.neves.fonseca.ti@gmail.com>
2023-05-26 17:28:09 -0300

    first commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,26 @@
-/* ------------------------------- Coloque sua lógica aqui ------------------------------- */
+function handleModalNextClass() {
+  let listNextClassButton = document.querySelectorAll(".header__btn-register-next-class");
+  let registerClassModal = document.querySelector(".header__next-class-modal-controller");
+
+  for (let i = 0; i < listNextClassButton.length; i++){
+    let button = listNextClassButton[i];
+    button.addEventListener("click", () => {
+      registerClassModal.showModal();
+      closeModal();
+    })
+  }
+}
+handleModalNextClass();
+
+function closeModal(){
+  let closeTabButton = document.querySelector(".header__btn-close-tab");
+  let registerButton = document.querySelector(".header__btn-register");
+  let registerClassModal = document.querySelector(".header__next-class-modal-controller");
+
+  closeTabButton.addEventListener('click', () => {
+    registerClassModal.close();
+  })
+  registerButton.addEventListener('click', () => {
+    registerClassModal.close();
+  })
+}
